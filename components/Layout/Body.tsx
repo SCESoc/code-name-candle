a687de090f2a5af461e1e3a0ddf6d0ecef46c7e5
dharshatharan <dharshatharan@gmail.com>
2021-11-06 00:32:10 -0400

    Update Body.tsx

--- components/Layout/Body.tsx
+++ components/Layout/Body.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Body({ children }: Props): ReactElement {
 	return (
-		<main className='bg-gray-100 md:flex md:self-center max-w-full md:max-w-4xl'>
+		<main className='bg-gray-100 w-full md:flex flex-grow md:self-center max-w-full md:max-w-4xl'>
 			{children}
 		</main>
 	)
